96421ed7537062274be8f9d0c495893793a50a6b
SandunLB <sandunlb2001@gmail.com>
2024-07-17 21:29:12 +0530

    commit #3

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
